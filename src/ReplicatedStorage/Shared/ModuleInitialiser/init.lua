382017a5e08298cbf36053418fcc50c062a4e566
KonouChan <wnaces52@gmail.com>
2023-04-15 16:43:48 +0100

    Module Initialiser #2
    Polishing it up.

--- src/ReplicatedStorage/Shared/ModuleInitialiser/init.lua
+++ src/ReplicatedStorage/Shared/ModuleInitialiser/init.lua
@@ -1,5 +1,8 @@
 -- Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+
+local isClient = RunService:IsClient()
 
 -- Shared
 local shared = ReplicatedStorage.Shared
@@ -14,32 +17,59 @@ local log = Log.new()
 
 local ModuleInitialiser = {}
 
+function ModuleInitialiser:GetLoadedModules()
+    return self._loadedModules
+end
+
+function ModuleInitialiser:GetLoadedModulesCount()
+    return self._loadedModulesCount
+end
+
+function ModuleInitialiser:GetErroredModulesCount()
+    return self._erroredModulesCount
+end
+
 function ModuleInitialiser:Start()
+    log:Log(false, `\n[{isClient and "Client" or "Server"}]: Starting...`)
     self.StartEvent:Fire()
 end
 
 function ModuleInitialiser:Require(module, _config)
     local startTime = os.clock()
-    log:Log(false, `[Server]: Initialising {module.Name}`)
-    local required = require(module)
+    log:Log(false, `[{isClient and "Client" or "Server"}]: Initialising {module.Name}`)
+    local success, err = pcall(function()
+        local required = require(module)
     required(setmetatable({}, {
-        __index = {_G = {
-            log = Log.new(),
-            net = self._globalenvironment,
-            Start = self.StartEvent
-        }}
-    }))
-    log:Log(false, `[Server]: Done! {math.floor(1000*(os.clock() - startTime) + 0.5)}ms`)
+        __index = {
+            _G = {
+                log = Log.new(),
+                net = self._globalenvironment,
+                Start = self.StartEvent
+            }}
+        }))
+    end)
+    if not success then
+        log:Warn(false, `\n[{isClient and "Client" or "Server"}]:\nError Initialising: {module.Name}\nIssue: {err}`)
+        table.insert(self._erroredModules, module.Name)
+        self._erroredModulesCount += 1
+        return
+    end
+    log:Log(false, `[{isClient and "Client" or "Server"}]: Done! {math.floor(1000*(os.clock() - startTime) + 0.5)}ms`)
+    self._loadedModules ..= `{module.Name}\n`
+    self._loadedModulesCount += 1
 end
 
-local function new(globalEnvironment)
+local function new(globalEnvironment, shouldLog)
     local self = {}
-
+    log:Enabled(shouldLog)
     -- // Module Event
     -- To be waited on until all modules have been required and initialised.
     -- When fired, it is safe to access global environment.
     self.StartEvent = SignalClass.new()
-
+    self._loadedModules = ""
+    self._loadedModulesCount = 0
+    self._erroredModulesCount = 0
+    self._erroredModules = {}
     self._globalenvironment = globalEnvironment
 
     return setmetatable(self, {
--- src/ReplicatedStorage/Shared/ModuleInitialiser/log.lua
+++ src/ReplicatedStorage/Shared/ModuleInitialiser/log.lua
@@ -1,19 +1,33 @@
 local log = {}
 
 function log:Log(pathName, toLog)
+    if not self._shouldLog then
+        return
+    end
     print(pathName and (`{debug.traceback()}`..`\n{toLog}\n`) or `{toLog}\n`)
 end
 
 function log:Warn(pathName, toWarn)
+    if not self._shouldLog then
+        return
+    end
     warn(pathName and (`{debug.traceback()}`..`\n{toWarn}\n`) or `{toWarn}\n`)
 end
 
 function log:Error(pathName, toError)
+    if not self._shouldLog then
+        return
+    end
     error(pathName and (`{debug.traceback()}`..`\n{toError}\n`) or `{toError}\n`)
 end
 
-local function new()
+function log:Enabled(bool)
+    self._shouldLog = bool
+end
+
+local function new(shouldLog)
     local self = {}
+    self._shouldLog = shouldLog or true
     return setmetatable(self, {
         __index = log
     })
