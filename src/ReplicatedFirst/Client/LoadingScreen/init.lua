92cc300f6804dd9d3aa1511789a73e585c4cf58b
KonouChan <wnaces52@gmail.com>
2023-04-19 21:17:13 +0100

    loading screen #2

---
+++ src/ReplicatedFirst/Client/LoadingScreen/init.lua
@@ -0,0 +1,6 @@
+local Players = game:GetService("Players")
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
+
+ReplicatedFirst:RemoveDefaultLoadingScreen()
+
+local player = Players.LocalPlayer
